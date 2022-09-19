66c68aa60c3db890ddd47aa3c2a6bf6688d7c196
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-09-19 13:44:33 +0200

    Init 002

--- Javascript 1/001_exercise.js
+++ Javascript 1/001_exercise.js
@@ -17,7 +17,7 @@ function hoursToSeconds(inHours) {
   let outSeconds = inHours * 60 * 60;
   return outSeconds
 }
-console.log(hoursToSeconds(25));
+console.log(hoursToSeconds(2));
 
 //Create a function that takes the users age in years and returns the age in days.
 
@@ -25,7 +25,7 @@ function yearsToDays(inYears) {
   let outDays = inYears * 365;
   return outDays
 }
-console.log(yearsToDays(24));
+console.log(yearsToDays(28));
 
 // && editorHasSelection (in keybindings.json)
 
@@ -35,7 +35,7 @@ console.log(yearsToDays(24));
 function returnTriangleArea(inBase, inHeight) {
   return (inBase * inHeight) / 2
 }
-console.log(returnTriangleArea(10, 14));
+console.log(returnTriangleArea(10, 10));
 
 //Write a function that converts days into minutes.
 
@@ -53,13 +53,43 @@ function returnFirstElement(inArray) {
 }
 console.log(returnFirstElement([1, 3, 5]));
 
+//för objekt
+
+function returnFirstKeyAndValueInObject(inObject) {
+  let key = Object.keys(inObject)[0];
+  let value = Object.values(inObject)[0];
+  return `key = ${key} and value = ${value}`
+}
+
+let myObject = {
+  name : "Batman",
+  favoriteFood: "tacos",
+};
+
+console.log(returnFirstKeyAndValueInObject(myObject));
+
+function isEven(num) {
+  /*
+  if(num % 2 === 0) {
+    return true
+  }
+  else {
+    return false
+  }
+  */
+  return num % 2 === 0
+}
+console.log(isEven(12));
+
+//Object.keys(ahash)[0];
+
 //You are counting points for a basketball game. Write a function that takes in 2 parameters - The amount of 2-pointers the team has scored, and the amount of 3-pointers. Return the total amount of points they’ve scored. (For example, finalscore(5,3) should return 19.)
 
 function totalPoints(in2Point, in3Point) {
   let outPoints = in2Point * 2 + in3Point * 3;
   return outPoints
 }
-console.log(totalPoints(10, 5));
+console.log(totalPoints(5, 3));
 
 //You’re helping a farmer count the total amount of legs among all his animals. He owns chickens (2 legs), cows(4 legs) and pigs (4 legs). Write a function that, given the amount of each animal, returns the total number of legs.
 
@@ -90,29 +120,42 @@ console.log(isDivisibleBy2(4));
 //Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
 
 function isLessOrEqualToZero(inNum) {
+  /*
   if (inNum <= 0) {
     return true
   }
   else {
     return false
   }
+  */
+ return inNum <= 0;
 }
-console.log(isLessOrEqualToZero(-1));
+console.log(isLessOrEqualToZero(1));
 
 //Given two numbers, return true if the sum of both numbers is less than 100. Otherwise return false.
 
 function isSumLessThan100(inX, inY) {
+  /*
   if (inX + inY < 100) {
     return true
   }
   else {
     return false
   }
+  */
+
+  //return inX + inY < 100;
+
+  return inX + inY < 100 ? true : inX + inY === 100 ? "exakt 100" : false
 }
-console.log(isSumLessThan100(40, 500));
+console.log(isSumLessThan100(50, 50));
 
 //Lycka till! /B
 
+let number = 20;
+// WTF
+// What ? True : False
+console.log(number < 33 ? "Lågt tal" : "Högt tal!");
 
 
 
---
+++ Javascript 1/002_övningar.js
@@ -0,0 +1,86 @@
+/*
+JavaScript-övningar #3 - Loopar & Array-metoder
+
+Skapa en funktion som loopar igenom en array, och returnera alla jämna tal (som en sträng eller som en ny array.)
+*/
+let myArray = [1,3,5,15,24,63,73,72,234];
+
+function evenNumbers(arr) {
+  let resultArray = [];
+  for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 === 0) {
+        resultArray.push(arr[i]);
+      }
+    }
+    return resultArray
+}
+console.log(evenNumbers(myArray));
+
+/*
+Skapa en funktion som loopar igenom en array, och multiplicerar alla tal med varandra. T.ex [2,3,4] ska funktionen returnera 24. (2*3*4 = 24)
+*/
+let myNumArray = [2, 3, 4];
+
+function multiplyNumbers(arr) {
+  let result = 1;
+  for (let i = 0; i < arr.length; i++) {
+    result *= arr[i];
+  }
+  return result
+}
+console.log(multiplyNumbers(myNumArray));
+
+/*
+Skapa en funktion som loopar igenom en array och kollar om den innehåller “jordgubbar”. Om den innehåller jordgubbar bör den returnera true, annars returneras false.
+*/
+let myFruitArray = ["bananer", "päron", "jordgubbar"];
+//let myFruitArray = ["bananer", "päron", "äpplen"];
+function jordgubbCheck(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "jordgubbar") {
+      return true
+    }
+  }
+  return false
+}
+
+console.log(jordgubbCheck(myFruitArray));
+
+/*
+Skapa en funktion som ska kunna ta in en array av siffror. Om arrayen har fler jämna tal än ojämna, returnera “Even array”. Om den har fler ojämna än jämna tal, returnera “Odd array”. Om den har lika många jämna som ojämna tal, returnera “Balanced array”.
+*/
+let numberArray = [];
+numberArray = [1, 2, 3];
+numberArray = [1, 2, 3, 4];
+//numberArray = [1, 2, 3, 4, 6];
+
+function numberChecker(arr) {
+  let evenNumbers = [];
+  let oddNumbers = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      evenNumbers.push(arr[i]);
+    }
+    else {
+      oddNumbers.push(arr[i]);
+    }
+  }
+  return evenNumbers.length > oddNumbers.length ? "Even array" : evenNumbers.length === oddNumbers.length ? "Balanced array" : "Odd array"
+}
+
+console.log(numberChecker(numberArray));
+
+/*
+Svår (Överkurs just nu - Läs på om array-metoden filter)
+Skapa en funktion som tar in en lista över förnamn, och filtrerar ut (dvs tar bort) alla namn som är längre än 6 bokstäver långa.
+*/
+let myNames = ["Brandon", "Tommy", "Henrik"];
+
+function filterNames(arr) {  
+  function isLessThanSixCharacters(name) {
+    return name.length < 6
+  }
+  return arr.filter(isLessThanSixCharacters)
+}
+
+console.log(filterNames(myNames));
