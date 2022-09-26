42e3f69c2e23ce12c6a113c155d413cd1aa54fa7
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-09-26 14:48:41 +0200

    remove 005 from kod.js

--- Javascript 1/Ankademin-Cafe/kod.js
+++ Javascript 1/Ankademin-Cafe/kod.js
@@ -73,19 +73,3 @@ let allP = document.querySelectorAll("p");
 allP.forEach(element => {
   element.style.color = "DarkRed";
 });
-
-
-
-
-
-
-
-
-let body = document.querySelector("body");
-let hejP = document.createElement("p");
-hejP.innerText = "Hej från JavaScript!";
-body.append(hejP);
-
-let myH1 = document.querySelector("h1");
-console.log(myH1);
-myH1.innerText("Rubrik frän JavaScript!");
