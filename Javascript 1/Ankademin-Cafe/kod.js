d93694ef73853b7b0fc6e824410b49ec7005ce53
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-09-26 14:31:03 +0200

    update kod.js

--- Javascript 1/Ankademin-Cafe/kod.js
+++ Javascript 1/Ankademin-Cafe/kod.js
@@ -1,17 +1,32 @@
+/*
 let cafeName = document.querySelector("#namn");
 cafeName.innerText = "Ankademin Café";
+*/
 
+document.querySelector("#namn").innerText = "Ankademin Café";
+
+/*
 let slogan = document.querySelector("#slogan");
 slogan.innerText = "Stockholms no.1 coding café!";
+*/
 
+document.querySelector("#slogan").innerText = "Stockholms no.1 coding café!";
+
+/*
 let html = document.querySelector("html");
 html.style.background = "MistyRose";
+*/
 
-let allProducts = document.querySelectorAll(".pris");
-allProducts.forEach(product => {
-  console.log(product);
-  product.innerText = "19 kr";
+document.body.style.background = "MistyRose";
+
+/*
+let allPrices = document.querySelectorAll(".pris");
+allPrices.forEach(price => {
+  console.log(price);
+  price.innerText = "19 kr";
 });
+*/
+document.querySelectorAll(".pris").forEach(price => price.innerText = "19 kr");
 
 let frukostMacka = document.querySelector(".produkt:nth-of-type(3) .pris");
 //frukostMacka.innerText = "Frukostmacka";
@@ -20,19 +35,35 @@ frukostMacka.innerText = "15 kr";
 //frukostMackaSpan.className = "pris";
 //frukostMacka.append(frukostMackaSpan);
 
+
 let myProduct = "Banan";
 let mySpan = document.createElement("span");
+//mySpan.classList.add("pris");
 mySpan.className = "pris";
-mySpan.innerText = " - 5 kr";
+
+let banan = {
+  name: "Banan",
+  price: 5
+}
 
 let lastLi = document.querySelector("ul li:last-child");
-lastLi.append(myProduct);
-lastLi.append(mySpan);
 
-let div = document.querySelector("div");
+/*
+lastLi.append(myProduct);
+mySpan.innerText = ` - ${banan.price} kr`;
+lastLi.append(mySpan);
+*/
+
+lastLi.innerHTML = `${banan.name} - <span class="pris">${banan.price} kr</span>`;
+
+//let div = document.querySelector("div");
+let div = document.querySelector("#website");
+/*
 let myH3 = document.createElement("h3");
 myH3.innerText = "Öppettider: Alla dagar kl 18-21";
 div.append(myH3);
+*/
+div.innerHTML += "<h3>Öppettider: Alla dagar kl 18-21</h3>";
 
 let myP = document.createElement("p");
 myP.innerText = "Till Ankademin Café är alla välkomna att sitta och koda och ta en mysig fika!";
