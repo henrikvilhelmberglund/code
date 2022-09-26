b4b5b4aa6b824bf1614dcf4de6759bcb2601dc92
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-09-26 13:36:55 +0200

    update

--- Javascript 1/005/005_övningar.js
+++ Javascript 1/005/005_övningar.js
@@ -6,3 +6,30 @@ body.append(hejP);
 let myH1 = document.querySelector("h1");
 console.log(myH1);
 myH1.innerText = "Rubrik från JavaScript!";
+
+let myButton = document.querySelector("#background-color-button");
+let myButton2 = document.createElement("button");
+myButton2.innerText = "New button";
+myButton2.className = "pColorButton";
+body.append(myButton2);
+
+
+function changeColor() {
+  body.style.background = rgb(Math.random()*255, Math.random()*255, Math.random()*255);
+  return true
+}
+
+function changePColor() {
+  let allP = document.querySelectorAll("p");
+  allP.forEach(element => {
+    element.style.color = rgb(Math.random()*255, Math.random()*255, Math.random()*255);  
+  });
+  return true
+}
+
+function rgb(r, g, b){
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+myButton.addEventListener("click", changeColor)  ;
+myButton2.addEventListener("click", changePColor);
