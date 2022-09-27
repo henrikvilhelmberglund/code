b6eea055b62d0a15127c6a996e5bfebc9c2aaff0
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-09-27 14:01:28 +0200

    add uppgift 3

--- Javascript 1/005/005_övningar.js
+++ Javascript 1/005/005_övningar.js
@@ -50,3 +50,85 @@ numArray.forEach((number, i) => {
   "blue";
   myUl.append(myIl);
 });
+
+/* Uppgift 3 */
+
+myObjectArray = [
+  {
+    name: "Batman",
+    age: 30,
+    amountOfPets: 0,
+    email: "bat@man.com",
+    favoriteAuthor: "Bill Finger and Bob Kane",
+    lovesWinter: false
+  },
+  {
+    name: "Superman",
+    age: 40,
+    amountOfPets: 1,
+    email: "super@man.com",
+    favoriteAuthor: "Jerry Siegel and Joe Shuster",
+    lovesWinter: false
+  },
+  {
+    name: "Spiderman",
+    age: 20,
+    amountOfPets: 3,
+    email: "spider@man.com",
+    favoriteAuthor: "Stan Lee and Steve Ditko",
+    lovesWinter: true
+}];
+
+let myDiv = document.createElement("div");
+myDiv.id = "profile-cards";
+body.append(myDiv);
+
+function reduceBlur(ul) {
+  ul.style.transition = "0.25s";
+  ul.style.filter = "blur(0px)";
+}
+
+function increaseBlur(ul) {
+  ul.style.transition = "0.25s";
+  ul.style.filter = "blur(10px)";
+}
+
+function createProfileCards(array) {
+  let result = document.querySelector("div ul");
+  if (!result) {
+    array.forEach(person => {
+      let myPersonUl = document.createElement("ul");
+      myPersonUl.style = `
+      border:6px solid black;
+      border-radius: 25px;
+      background-color: lavender;
+      width: 300px;
+      padding: 10px;
+      filter: drop-shadow(1px 5px 7px);
+      filter: blur(10px);
+      `;
+      myPersonUl.addEventListener("mouseenter", () => reduceBlur(myPersonUl));
+      myPersonUl.addEventListener("mouseleave", () => increaseBlur(myPersonUl));
+      myDiv.append(myPersonUl);
+      myPersonUl.innerHTML += 
+      `
+      <li class="name">${person.name}</li>
+      <li class="age">${person.age} years old</li>
+      <li class="pets">${
+      person.amountOfPets === 0 ? `no pets` :
+      person.amountOfPets === 1 ? `${person.amountOfPets} pet` :
+      `${person.amountOfPets} pets`}</li>
+      <li class="email">${person.email}</li>
+      <li class="favorite-author">Favorite authors: ${person.favoriteAuthor}</li>
+      <li class="loves-winte">${person.lovesWinter ? `${person.name} loves Winter!` : `${person.name} does not enjoy Winter.`}</li>
+      `
+    });
+  }
+}
+
+let createProfileCardsButton = document.createElement("button");
+myDiv.append(createProfileCardsButton);
+createProfileCardsButton.innerText = "Create profile cards";
+createProfileCardsButton.addEventListener("click", () => createProfileCards(myObjectArray));
+//for debugging
+createProfileCards(myObjectArray);
