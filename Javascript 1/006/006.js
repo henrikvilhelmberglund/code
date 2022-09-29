03e9bd454da87b637ae8837ce64b594353e0d60f
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-09-29 10:32:40 +0200

    add 006

---
+++ Javascript 1/006/006.js
@@ -0,0 +1,38 @@
+const cities = [
+  {
+    city: "London",
+    code: "UK",
+    population: 11504632,
+    foodRanking: 4
+  },
+  {
+    city: "Manchester",
+    code: "UK",
+    population: 4280424,
+    foodRanking: 5
+  },
+  {
+    city: "Paris",
+    code: "FR",
+    population: 10830024,
+    foodRanking: 9
+  },
+  {
+    city: "Nice",
+    code: "FR",
+    population: 4250024,
+    foodRanking: 10
+  },
+  {
+    city: "Berlin",
+    code: "DE",
+    population: 8250024,
+    foodRanking: 8
+  },
+  {
+    city: "Munich",
+    code: "DE",
+    population: 5250050,
+    foodRanking: 6
+  }
+];
