ab384d9e1a074f78b252eaa816a744b220f2dd6c
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-10-03 21:32:48 +0200

    Add variables for query selectors

---
+++ Javascript 1/007/HockeyRewrite/main.js
@@ -0,0 +1,55 @@
+// HTML structure below
+/*
+
+        <!-- Lag A-div -->
+        <div id="lag-a-div">
+            <h2 id="lag-a-h2"></h2>
+            <input type="text" id="lag-a-input-field" />
+            <button id="lag-a-add-player-button">Add player</button>
+            <ul id="lag-a-ul"></ul>
+        </div>
+        <!-- Lag B-div -->
+        <div id="lag-b-div">
+            <h2 id="lag-b-h2"></h2>
+            <input type="text" id="lag-b-input-field" />
+            <button id="lag-a-add-player-button">Add player</button>
+            <ul id="lag-b-ul"></ul>
+        </div>
+
+*/
+
+let lagADiv = document.querySelector("#lag-a-div");
+let lagBDiv = document.querySelector("#lag-b-div");
+let lagAh2 = document.querySelector("#lag-a-h2");
+let lagBh2 = document.querySelector("#lag-b-h2");
+let lagAInputField = document.querySelector("#lag-a-input-field");
+let lagBInputField = document.querySelector("#lag-b-input-field");
+let lagAAddPlayerButton = document.querySelector("#lag-a-ad-player-button");
+let lagBAddPlayerButton = document.querySelector("#lag-a-ad-player-button");
+
+
+
+// 1.
+/* 
+Done in HTML but would be done like this in JS
+
+let lagAInputField = document.createElement("input");
+lagAInputField.id = "lag-a-input-field";
+lagADiv.append(lagAInputField);
+*/
+
+
+
+
+// 2.
+
+// 3.
+
+// 4.
+
+// 5.
+
+// 6.
+
+// 7.
+
