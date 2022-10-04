892b7cf9047b544a084dff0b4fbab12546287778
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-10-04 15:46:21 +0200

    fix typos

--- Javascript 1/007/HockeyRewrite/main.js
+++ Javascript 1/007/HockeyRewrite/main.js
@@ -12,11 +12,13 @@
         <div id="lag-b-div">
             <h2 id="lag-b-h2"></h2>
             <input type="text" id="lag-b-input-field" />
-            <button id="lag-a-add-player-button">Add player</button>
+            <button id="lag-b-add-player-button">Add player</button>
             <ul id="lag-b-ul"></ul>
         </div>
 
 */
+
+console.log("testing!");
 
 let lagADiv = document.querySelector("#lag-a-div");
 let lagBDiv = document.querySelector("#lag-b-div");
@@ -24,13 +26,13 @@ let lagAh2 = document.querySelector("#lag-a-h2");
 let lagBh2 = document.querySelector("#lag-b-h2");
 let lagAInputField = document.querySelector("#lag-a-input-field");
 let lagBInputField = document.querySelector("#lag-b-input-field");
-let lagAAddPlayerButton = document.querySelector("#lag-a-ad-player-button");
-let lagBAddPlayerButton = document.querySelector("#lag-a-ad-player-button");
+let lagAAddPlayerButton = document.querySelector("#lag-a-add-player-button");
+let lagBAddPlayerButton = document.querySelector("#lag-b-add-player-button");
 
 
 
 // 1.
-/* 
+/*
 Done in HTML but would be done like this in JS
 
 let lagAInputField = document.createElement("input");
