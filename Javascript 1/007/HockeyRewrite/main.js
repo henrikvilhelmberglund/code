5972e8b18642d8e38d77f372b2f84724ad41f49e
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-10-04 19:22:28 +0200

    update

--- Javascript 1/007/HockeyRewrite/main.js
+++ Javascript 1/007/HockeyRewrite/main.js
@@ -18,17 +18,14 @@
 
 */
 
-
 let lagADiv = document.querySelector("#lag-a-div");
 let lagBDiv = document.querySelector("#lag-b-div");
 let lagAh2 = document.querySelector("#lag-a-h2");
 let lagBh2 = document.querySelector("#lag-b-h2");
 let lagAInputField = document.querySelector("#lag-a-input-field");
-let lagBInputField = document.querySelector("#lag-b-input-field");
 let lagAAddPlayerButton = document.querySelector("#lag-a-add-player-button");
-let lagBAddPlayerButton = document.querySelector("#lag-b-add-player-button");
-
-
+let lagAUl = document.querySelector("#lag-a-ul");
+let lagBUl = document.querySelector("#lag-b-ul");
 
 // 1.
 /*
@@ -44,9 +41,46 @@ lagADiv.append(lagAInputField);
 
 // 2.
 
-// 3.
+function leaveTeam(event, li) {
+  li.parentNode.remove(li);
+}
 
-// 4.
+function changeTeam(event, li) {
+  console.log("hi");
+}
+
+function addPlayer(event) {
+  let li = document.createElement("li");
+  li.innerText = lagAInputField.value;
+  let lagAMembers = document.querySelectorAll("#lag-a-ul li");
+  let lagBMembers = document.querySelectorAll("#lag-b-ul li");
+
+  // 5.
+  let leaveTeamButton = document.createElement("button");
+  leaveTeamButton.innerText = "Leave Team";
+  leaveTeamButton.addEventListener("click", (event) => leaveTeam(event, li));
+
+  // 7.
+  let changeTeamButton = document.createElement("button");
+  changeTeamButton.innerText = "Change Teamname";
+  changeTeamButton.addEventListener("click", (event) => changeTeam(event, li));
+  // 3.
+  if (lagAMembers.length < 5) {
+    lagAUl.append(li);
+    li.append(leaveTeamButton);
+    li.append(changeTeamButton);
+  }
+  // 4.
+  else if (lagAMembers.length >= 5 && lagBMembers.length < 5) {
+    lagBUl.append(li);
+    li.append(leaveTeamButton);
+    li.append(changeTeamButton);
+  }
+}
+
+lagAAddPlayerButton.addEventListener("click", (event) => addPlayer(event));
+
+
 
 // 5.
 
