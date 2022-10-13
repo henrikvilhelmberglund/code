bc6992df702ca4da97926deb02cd520c159d6193
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-10-13 10:46:27 +0200

    update

---
+++ Javascript 1/009/main.js
@@ -0,0 +1,82 @@
+let clothesArray = [
+  {
+    name: "Cute t-shirt",
+    type: "t-shirt",
+    color: "pink",
+    brand: "Japan"
+  },
+  {
+    name: "Stylish hat",
+    type: "hat",
+    color: "white",
+    brand: "Stetson"
+  },
+  {
+    name: "Comfy sweater",
+    type: "sweater",
+    color: "blue",
+    brand: "The comfy"
+  },
+  {
+    name: "Warm winter jacket",
+    type: "jacket",
+    color: "black",
+    brand: "Peak Performance"
+  },
+  {
+    name: "Cool sneakers",
+    type: "shoes",
+    color: "white",
+    brand: "Nike"
+  }
+];
+
+let showAllProductsBtn = document.querySelector("#show-all-products-btn");
+let showProductsUl = document.querySelector("#show-products-ul");
+let showSelectedProductsBtn = document.querySelector("#show-selected-products-btn");
+let selectedDropdown;
+let selectedRadio;
+let checkbox = document.querySelector("#checkbox");;
+showAllProductsBtn.addEventListener("click", () => showAllProducts(clothesArray));
+showSelectedProductsBtn.addEventListener("click", () => showSelectedProducts(clothesArray));
+
+function showAllProducts(clothes) {
+  showProductsUl.innerHTML = "";
+  clothes.forEach(cloth => {
+    let li = document.createElement("li");
+    li.innerText = cloth.name;
+    showProductsUl.append(li);
+
+  });
+}
+
+function isNotRobotCheck() {
+  if (checkbox.checked === true) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function showSelectedProducts(clothes) {
+  showProductsUl.innerHTML = "";
+  if (!isNotRobotCheck()) {
+    alert("Please confirm you're not a robot!");
+    return false;
+  }
+  selectedDropdown = document.querySelector("#types").value;
+  selectedRadio = document.querySelector("[type=radio]:checked");
+  clothes = clothes.filter(cloth => filterClothes(cloth));
+  clothes.forEach(cloth => {
+    let li = document.createElement("li");
+    li.innerText = cloth.name;
+    showProductsUl.append(li);
+
+  });
+}
+
+function filterClothes(cloth) {
+  return cloth.type === selectedDropdown.toLowerCase() &&
+    cloth.brand === selectedRadio.value;
+}
--- Javascript 1/009/readme.md
+++ Javascript 1/009/readme.md
@@ -11,13 +11,15 @@ Skapa en array med minst fem objekt. Varje objekt ska ha ett name, type, brand o
   brand: ‘Nike’
 }
 
-Skapa en “Show products”-knapp som skriver ut en lista i DOM:en med samtliga klädesplagg.
+*Skapa en “Show products”-knapp som skriver ut en lista i DOM:en med samtliga klädesplagg.
 
-Skapa en dropdown för olika klädtyper (jacka,skor, etc). Skapa sedan en filter-knapp som när du klickar på den gör att  du skriver ut i DOM:en, där du endast skriver ut namnen på de objekten som har den valda typen.
 
-Skapa ytterligare en grupp radio-buttons för olika klädmärken (Nike, Gucci, Adidas etc). När du nu klickar på filterknappen (samma knapp som tidigare, skapa inte en ny) ska listan över kläderna filtreras baserat på både typ och märke.
+*Skapa en dropdown för olika klädtyper (jacka,skor, etc). Skapa sedan en filter-knapp som när du klickar på den gör att  du skriver ut i DOM:en, där du endast skriver ut namnen på de objekten som har den valda typen.
 
-Skapa en checkbox med label “I am not a robot” som måste vara ifylld för att kunna filtrera kläderna. Om checkboxen inte är ifylld, skapa en alert med texten “Please confirm you’re not a robot!”.
+
+*Skapa ytterligare en grupp radio-buttons för olika klädmärken (Nike, Gucci, Adidas etc). När du nu klickar på filterknappen (samma knapp som tidigare, skapa inte en ny) ska listan över kläderna filtreras baserat på både typ och märke.
+
+*Skapa en checkbox med label “I am not a robot” som måste vara ifylld för att kunna filtrera kläderna. Om checkboxen inte är ifylld, skapa en alert med texten “Please confirm you’re not a robot!”.
 
 (Svår) Skapa en checkbox för varje färg som du har använt för dina kläder. Försök lista ut hur du kan visa endast de kläder som har en färg vars checkbox är ifyllda. Lycka till!
 Extra - Byt ut dina radio-buttons mot checkboxar för varje alternativ!
