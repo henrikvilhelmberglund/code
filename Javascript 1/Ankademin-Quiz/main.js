0e006823465b7c976a154615b9e43dc33bd8420b
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-10-19 00:12:47 +0200

    add more text

--- Javascript 1/Ankademin-Quiz/main.js
+++ Javascript 1/Ankademin-Quiz/main.js
@@ -17,14 +17,26 @@ A. Yes B. No
 Is the owner of the Urban Rescue Ranch called Ben?
 A. Yes B. No
 
+Are the animals cute?
+A. Yes B. No
+
+Has any animal ever escaped?
+A. Yes B. No
+
+Is it possible to catch a goose and bring it home with you?
+A. Yes B. No
+
+Is the Urban Rescue Ranch full now?
+A. Yes B. No
+
 Select the ratite species present at the Urban Rescue Ranch.
 -Ostrich -Emu -Rhea -Kiwi 
 
 Select the flying bird species present at the Urban Rescue Ranch.
--Call duck -Chicken -Goose -Flamingo
+-Call duck -Hawk -Goose -Flamingo
 
 What was at the location before it became the Urban Rescue Ranch?
-A. A field B. A gas station C. A farm D. A crackhouse
+A. A field B. A gas station C. A crackhouse  D. A farm
 
 What does the owner of the Urban Rescue Ranch like to add before his name?
 A. Farmer B. Mister C. Ranger D. Uncle
