5b032f34e9e6280a2338d8c17adad62a531da7b8
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2022-10-18 17:30:33 +0200

    add questions for adding to  data later

--- Javascript 1/Anademin-Quiz/main.js
+++ Javascript 1/Anademin-Quiz/main.js
@@ -1,9 +1,45 @@
+/*
+Are there hedgehogs at the Urban Rescue Ranch?
+A. Yes B. No
+
+Are there cats at the Urban Rescue Ranch?
+A. Yes B. No
+
+Are there horses at the Urban Rescue Ranch?
+A. Yes B. No
+
+Are there pigs at the Urban Rescue Ranch?
+A. Yes B. No
+
+Are there kangaroos at the Urban Rescue Ranch?
+A. Yes B. No
+
+Is the owner of the Urban Rescue Ranch called Ben?
+A. Yes B. No
+
+Select the ratite species present at the Urban Rescue Ranch.
+-Ostrich -Emu -Rhea -Kiwi 
+
+Select the flying bird species present at the Urban Rescue Ranch.
+-Call duck -Chicken -Goose -Flamingo
+
+What was at the location before it became the Urban Rescue Ranch?
+A. A field B. A gas station C. A farm D. A crackhouse
+
+What does the owner of the Urban Rescue Ranch like to add before his name?
+A. Farmer B. Mister C. Ranger D. Uncle
+
+What is the goal the owner of the Urban Rescue Ranch has for his animal friend DaBaby?
+A. To live a peaceful and happy life B. To run from the edge to the other edge in 10 seconds C. To fight Jake Paul in a boxing match  D. To win a prize in an exhibition
+
+*/
+
 const questions = [
   {
     questionType: "trueFalse",
     // multipleChoice
     // checkboxes
     question: "",
-    correctAnswer: "",
+    answers: "",
   },
 ];
